901d9cd6e9a87c3b869805139b8a59ae5e3895b0
James Reyes <jamesleoreyes@gmail.com>
2025-10-09 00:15:36 -0400

    chore: Add initial project structure with main application logic and dependencies
    - Created .gitignore to exclude Python artifacts and virtual environments.
    - Added .python-version to specify Python version 3.12.
    - Implemented main.py for content generation using Google GenAI API.
    - Defined project metadata and dependencies in pyproject.toml.
    - Initialized uv.lock for dependency management.

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get('GEMINI_API_KEY')
+client = genai.Client(api_key=api_key)
+
+if len(sys.argv) > 1:
+    prompt = sys.argv[1]
+else:
+    print('ERROR: A prompt must be passed as a positional argument')
+    sys.exit(1)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=prompt
+)
+
+print(response.text)
+print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+print(f'Response tokens: ${response.usage_metadata.candidates_token_count}')
