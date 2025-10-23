30e04107cc1132f2d08e208abff5867dd1e15796
James Reyes <jamesleoreyes@gmail.com>
2025-10-23 00:19:11 -0400

    refactor: Update main.py to allow conversation handling and message formatting instead of "one-off" prompts
    - Renamed variable 'prompt' to 'user_prompt' for clarity.
    - Introduced a structured message format using 'types.Content' and 'types.Part' for better compatibility with the Google GenAI API.
    - Updated the content generation call to use the new message structure.

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get('GEMINI_API_KEY')
 client = genai.Client(api_key=api_key)
 
+
 if len(sys.argv) > 1:
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
 else:
     print('ERROR: A prompt must be passed as a positional argument')
     sys.exit(1)
 
+messages = [
+    types.Content(role='user', parts=[types.Part(text=user_prompt)])
+]
+
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents=prompt
+    contents=messages
 )
 
 print(response.text)
